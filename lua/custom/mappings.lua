f02c26bae718427da37008d7dbc6372fc417589f
lndl-hlaw <zgubaisildura@o2.pl>
2025-02-03 20:44:45 +0100

    Remove unnecessary mapping for switching buffers. w

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,7 +5,6 @@ M.dap = {
   n = {
     ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>", "Add breakpoint at line" },
     ["<leader>dr"] = { "<cmd> DapContinue <CR>", "Start or continue the debugger" },
-    ["<C-u>"] = { "<cmd> bprevious <CR>", "Switch to previous buffer" },
     ["<S-Up>"] = { "<cmd> m-2 <CR>", "Moves one line up" },
     ["<S-Down>"] = { "<cmd> m+ <CR>", "Moves one line down" },
     ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
