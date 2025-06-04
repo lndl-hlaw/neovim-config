2a2bd3505a3339fb6c1d5112fa226ddcfc2e5abe
lndl-hlaw <zgubaisildura@o2.pl>
2025-06-04 20:04:14 +0200

    Large changes in telescope

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,16 +1,226 @@
 local plugins = {
+  { "nvim-telescope/telescope-fzf-native.nvim",    build = "make" },
+  { "nvim-telescope/telescope-ui-select.nvim" },
+  { "debugloop/telescope-undo.nvim" },
+  { "nvim-telescope/telescope-live-grep-args.nvim" },
+  {
+      "exosyphon/telescope-color-picker.nvim",
+      config = function()
+          vim.keymap.set("n", "<leader>uC", "<cmd>Telescope colors<CR>", { desc = "Telescope Color Picker" })
+      end,
+  },
+  {
+    "aaronhallaert/advanced-git-search.nvim",
+    dependencies = {
+      "tpope/vim-fugitive",
+      "tpope/vim-rhubarb",
+    },
+  },
+  -- {
+  --   "folke/noice.nvim",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     routes = {
+  --       {
+  --         filter = { event = "notify", find = "No information available" },
+  --         opts = { skip = true },
+  --       },
+  --     },
+  --     presets = {
+  --       lsp_doc_border = true,
+  --     },
+  --   },
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "rcarriga/nvim-notify",
+  --   },
+  -- },
+  {
+    "AckslD/nvim-neoclip.lua",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" },
+    },
+     config = function()
+      require("neoclip").setup({
+        history = 1000,
+        enable_persistent_history = false,
+        length_limit = 1048576,
+        continuous_sync = false,
+        db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
+        filter = nil,
+        preview = true,
+        prompt = nil,
+        default_register = '"',
+        default_register_macros = "q",
+        enable_macro_history = true,
+        content_spec_column = false,
+        disable_keycodes_parsing = false,
+        on_select = {
+          move_to_front = false,
+          close_telescope = true,
+        },
+        on_paste = {
+          set_reg = false,
+          move_to_front = false,
+           close_telescope = true,
+        },
+        on_replay = {
+          set_reg = false,
+          move_to_front = false,
+          close_telescope = true,
+        },
+        on_custom_action = {
+          close_telescope = true,
+        },
+        keys = {
+          telescope = {
+            i = {
+              select = "<cr>",
+              paste = "<c-j>",
+              paste_behind = "<c-k>",
+              replay = "<c-q>", -- replay a macro
+              delete = "<c-d>", -- delete an entry
+              edit = "<c-e>", -- edit an entry
+              custom = {},
+            },
+            n = {
+              select = "<cr>",
+              paste = "p",
+              --- It is possible to map to more than one key.
+              -- paste = { 'p', '<c-p>' },
+              paste_behind = "P",
+              replay = "q",
+              delete = "d",
+              edit = "e",
+              custom = {},
+            },
+          },
+        },
+      })
+      vim.keymap.set("n", "<leader>o", "<cmd>Telescope neoclip<CR>", { desc = "Telescope Neoclip" })
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.4",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+      vim.keymap.set(
+        "n",
+        "<leader>fg",
+        "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+        { desc = "Live Grep" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>fc",
+        '<cmd>lua require("telescope.builtin").live_grep({ glob_pattern = "!{spec,test}"})<CR>',
+        { desc = "Live Grep Code" }
+      )
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help Tags" })
+      vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find Symbols" })
+      vim.keymap.set("n", "<leader>fi", "<cmd>AdvancedGitSearch<CR>", { desc = "AdvancedGitSearch" })
+      vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find Old Files" })
+      vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find Word under Cursor" })
+      vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Search Git Commits" })
+      vim.keymap.set("n", "<leader>gb", builtin.git_bcommits, { desc = "Search Git Commits for Buffer" })
+      vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })		
+      vim.keymap.set("n", "<leader>/", function()
+        -- You can pass additional configuration to telescope to change theme, layout, etc.
+        require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+          winblend = 10,
+          previewer = false,
+          layout_config = { width = 0.7 },
+        }))
+      end, { desc = "[/] Fuzzily search in current buffer" })
+      local telescope = require("telescope")
+      local telescopeConfig = require("telescope.config")
+      -- Clone the default Telescope configuration
+      local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+      -- I want to search in hidden/dot files.
+      table.insert(vimgrep_arguments, "--hidden")
+      -- I don't want to search in the `.git` directory.
+      table.insert(vimgrep_arguments, "--glob")
+      table.insert(vimgrep_arguments, "!**/.git/*")
+      local actions = require("telescope.actions")
+      local select_one_or_multi = function(prompt_bufnr)
+        local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+        local multi = picker:get_multi_selection()
+        if not vim.tbl_isempty(multi) then
+          require("telescope.actions").close(prompt_bufnr)
+          for _, j in pairs(multi) do
+            if j.path ~= nil then
+              vim.cmd(string.format("%s %s", "edit", j.path))
+            end
+          end
+        else
+          require("telescope.actions").select_default(prompt_bufnr)
+        end
+      end
+      telescope.setup({
+        defaults = {
+          -- `hidden = true` is not supported in text grep commands.
+          vimgrep_arguments = vimgrep_arguments,
+          path_display = { "truncate" },
+          mappings = {
+            n = {
+              ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+            i = {
+              ["<C-j>"] = actions.cycle_history_next,
+              ["<C-k>"] = actions.cycle_history_prev,
+              ["<CR>"] = select_one_or_multi,
+              ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist,
+              ["<C-S-d>"] = actions.delete_buffer,
+  				    ["<C-s>"] = actions.cycle_previewers_next,
+  				    ["<C-a>"] = actions.cycle_previewers_prev,
+            },
+          },
+        },
+        pickers = {
+          find_files = {
+            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+          },
+        },
+        extensions = {
+          undo = {
+            use_delta = true,
+            use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
+            side_by_side = false,
+            vim_diff_opts = { ctxlen = vim.o.scrolloff },
+            entry_format = "state #$ID, $STAT, $TIME",
+            mappings = {
+              i = {
+                ["<C-cr>"] = require("telescope-undo.actions").yank_additions,
+                ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+                ["<cr>"] = require("telescope-undo.actions").restore,
+              },
+            },
+          },
+        },
+      })
+      require("telescope").load_extension("neoclip")
+      require("telescope").load_extension("fzf")
+      require("telescope").load_extension("ui-select")
+      vim.g.zoxide_use_select = true
+      require("telescope").load_extension("undo")
+      require("telescope").load_extension("advanced_git_search")
+      require("telescope").load_extension("live_grep_args")
+      require("telescope").load_extension("colors")
+      -- require("telescope").load_extension("noice")
+    end,
+  },
   {
     "numToStr/Comment.nvim",
     config = function()
       require('Comment').setup()
     end
-  },
-  { "nvim-telescope/telescope-fzf-native.nvim",    build = "make" },
-  {
-    "exosyphon/telescope-color-picker.nvim",
-    config = function()
-      vim.keymap.set("n", "<leader>uC", "<cmd>Telescope colors<CR>", { desc = "Telescope Color Picker" })
-    end,
   },
   {
     "mbbill/undotree",
@@ -18,13 +228,6 @@ local plugins = {
       vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<CR>", { desc = "Telescope Undo" })
     end,
   },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
-    "debugloop/telescope-undo.nvim",
-  },
-  { "nvim-telescope/telescope-live-grep-args.nvim" },
   {
     "nvim-lua/plenary.nvim",
     lazy=true
