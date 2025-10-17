7cb3c240e5c92c301c92236fb0be44bff658112e
lndl-hlaw <hubert.lawenda@proton.me>
2025-10-17 18:47:23 +0200

    Tidy up in lspconfigs

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,37 +1,35 @@
 local base = require("plugins.configs.lspconfig")
-local on_attach = base.on_attach
+local on_attach_base = base.on_attach or function() end
 local on_init = base.on_init
 local capabilities = base.capabilities
 
 local lspconfig = require("lspconfig")
 
--- LSP for c++ clangd
+-- LSP for clangd
 lspconfig.clangd.setup {
   on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
-    on_attach(client, bufnr)
+    -- Call the original on_attach safely
+    on_attach_base(client, bufnr)
   end,
   capabilities = capabilities,
 }
 
--- LSP for haskell
+-- LSP for Haskell
 lspconfig.hls.setup {
-  on_attach = on_attach,
+  on_attach = on_attach_base,
   on_init = on_init,
   capabilities = capabilities,
   filetypes = { 'haskell', 'lhaskell', 'cabal'},
 }
 
--- LSP for python
-local python_servers = {
-  "pyright",
-  --"ruff_lsp",
-}
+-- Python LSP
+local python_servers = { "pyright" }
 
 for _, lsp in ipairs(python_servers) do
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
+  lspconfig[lsp].setup {
+    on_attach = on_attach_base,
     capabilities = capabilities,
-    filetypes = {"python"},
-  })
+    filetypes = { "python" },
+  }
 end
