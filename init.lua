e669712ad5bc293fee40a3f60c5a9d3786a4638a
lndl-hlaw <hubert.lawenda@proton.me>
2025-10-17 20:59:07 +0200

    Add some plugins and mappings for telescope and LSP

--- init.lua
+++ init.lua
@@ -21,21 +21,26 @@ vim.keymap.set('n', '<leader>q', ":q<CR>")                  -- Quit the file
 
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')	-- Switch to alternate file (recently opened)
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')  -- Switch to alternate file (recently opened)
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>') -- The same as above, but instead of switching, it splits the screen horizontally
 
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/echasnovski/mini.pick" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },      -- Some async library for neovim lua.
 	{ src = "https://github.com/MunifTanjim/nui.nvim" },       -- This is an UI tool for nvim, higly customizable, may be used by another exts.
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" }, -- This is a pack of icons for file explorers, trees, etc.
+	{ src = "https://github.com/nvim-telescope/telescope.nvim",          version = "0.1.8" },
+	{ src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
+	{ src = "https://github.com/LinArcX/telescope-env.nvim" },  -- Displays current environmental variables with `:Telescope env` command
+	{ src = "https://github.com/aznhe21/actions-preview.nvim" }, -- Preview on action on binding
+	{ src = "https://github.com/numToStr/Comment.nvim" },       -- Commenting on 'gc'
+	{ src = "https://github.com/nvimtools/none-ls.nvim" },
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
 })
 
-require "mini.pick".setup()
 require "oil".setup()
 require "nvim-treesitter.configs".setup({
 	ensure_installed = { "python", "cpp", "rust", "haskell", "cuda", "lua" },
@@ -46,8 +51,9 @@ require "nvim-treesitter.configs".setup({
 	modules = {}
 })
 
-vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
-vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
+require "null-ls".setup({
+	require "custom.configs.null-ls"
+})
 vim.keymap.set('n', '<leader>e', ':Oil<CR>')
 
 -- LSP configuration:
@@ -82,3 +88,78 @@ vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
+
+
+-- Telscope configuration
+local telescope = require("telescope")
+telescope.setup({
+	defaults = {
+		preview = { treesitter = false },
+		color_devicons = true,
+		sorting_strategy = "ascending",
+		borderchars = {
+			"─", -- top
+			"│", -- right
+			"─", -- bottom
+			"│", -- left
+			"┌", -- top-left
+			"┐", -- top-right
+			"┘", -- bottom-right
+			"└", -- bottom-left
+		},
+		path_displays = { "smart" },
+		layout_config = {
+			height = 100,
+			width = 400,
+			prompt_position = "top",
+			preview_cutoff = 40,
+		}
+	}
+})
+telescope.load_extension("ui-select")
+
+require("actions-preview").setup {
+	backend = { "telescope" },
+	extensions = { "env" },
+	telescope = vim.tbl_extend(
+		"force",
+		require("telescope.themes").get_dropdown(), {}
+	)
+}
+
+local builtin = require("telescope.builtin")
+
+-- Telescope and actions-preview bindings:
+vim.keymap.set({ "n" }, "<leader>f", builtin.find_files, { desc = "Telescope live grep" })
+vim.keymap.set({ "n" }, "<leader>g", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set({ "n" }, "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set({ "n" }, "<leader>si", builtin.grep_string, { desc = "Telescope live string" })
+vim.keymap.set({ "n" }, "<leader>so", builtin.oldfiles, { desc = "Telescope buffers" })
+vim.keymap.set({ "n" }, "<leader>sh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set({ "n" }, "<leader>sm", builtin.man_pages, { desc = "Telescope man pages" })
+vim.keymap.set({ "n" }, "<leader>sr", builtin.lsp_references, { desc = "Telescope tags" })
+vim.keymap.set({ "n" }, "<leader>st", builtin.builtin, { desc = "Telescope tags" })
+vim.keymap.set({ "n" }, "<leader>sd", builtin.registers, { desc = "Telescope tags" })
+vim.keymap.set({ "n" }, "<leader>sc", builtin.git_bcommits, { desc = "Telescope tags" })
+vim.keymap.set({ "n" }, "<leader>se", "<cmd>Telescope env<cr>", { desc = "Telescope tags" })
+vim.keymap.set({ "n" }, "<leader>sa", require("actions-preview").code_actions)
+vim.keymap.set({ "n" }, 'gr', builtin.lsp_references, { noremap = true, silent = true })
+vim.keymap.set({ "n" }, 'gd', builtin.lsp_definitions, { noremap = true, silent = true })
+vim.keymap.set({ "n" }, 'gi', builtin.lsp_implementations, { noremap = true, silent = true })
+vim.keymap.set({ "n" }, 'gt', builtin.lsp_type_definitions, { noremap = true, silent = true })
+
+require "lualine".setup {
+	options = {
+		theme = 'everforest', -- pick your theme here
+		section_separators = '',
+		component_separators = '',
+	},
+	sections = {
+		lualine_a = {
+			{
+				'buffers',
+				show_filename_only = false
+			}
+		}
+	}
+}
